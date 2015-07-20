87dddfdcb1882b21d743c609af46f93531716b33
Nile Tate <Nilejt@gmail.com>
2015-07-20 11:31:58 -0400

    Addition to the code
    Updates include an addition of a view and a model.

--- MVCmovieDEMO/Controllers/HelloWorldController.cs
+++ MVCmovieDEMO/Controllers/HelloWorldController.cs
@@ -18,9 +18,13 @@ namespace MVCmovieDEMO.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 4)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+
         }
 
         
